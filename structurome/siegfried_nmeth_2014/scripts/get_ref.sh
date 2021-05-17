c8146be8b82275eb65dde68f7a10ccb667be10c6
Kah Wai LIM <kahwai222@gmail.com>
2021-05-18 03:46:28 +0800

    initial setup of analysis for Siegfried et al., Nature Methods 2014

---
+++ structurome/siegfried_nmeth_2014/scripts/get_ref.sh
@@ -0,0 +1,12 @@
+#
+# This script downloads the reference HIV-1 (strain NL4-3) genome to the refs folder.
+#
+
+# Stop on any error and print all commands.
+set -uex
+
+# Create refs directory if it does not already exist.
+mkdir -p refs
+
+# Fetch fasta file of HIV-1 genome.
+efetch -db nucleotide -id M19921.2 -format fasta -seq_start 455 -seq_stop 9627 > refs/HIV.fa
---
+++ structurome/siegfried_nmeth_2014/scripts/get_sra.sh
@@ -0,0 +1,20 @@
+#
+# This script downloads the reference HIV-1 (strain NL4-3) genome to the refs folder.
+#
+
+# Stop on any error and print all commands.
+set -uex
+
+# Fetch sequencing reads from SRA.
+# cat ids.txt | parallel "fastq-dump --split-files {}"
+
+# Create reads directory if it does not already exist.
+mkdir -p reads
+
+# Move fastq files to reads directory and rename accordingly.
+mv SRR1301979_1.fastq reads/Denatured_R1.fastq
+mv SRR1301979_2.fastq reads/Denatured_R2.fastq
+mv SRR1301974_1.fastq reads/1M7_R1.fastq
+mv SRR1301974_2.fastq reads/1M7_R2.fastq
+mv SRR1301978_1.fastq reads/Untreated_R1.fastq
+mv SRR1301978_2.fastq reads/Untreated_R2.fastq
